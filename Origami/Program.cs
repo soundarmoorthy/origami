822779d747d397abda19b5195603c016a738768f
Soundararajan D <dakshins@optum.com>
2020-08-29 23:52:19 +0530

    Initial commit

---
+++ Origami/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Origami
+{
+    class Program
+    {
+        static string jsonFile =
+            "/Users/sdhaksh5/Desktop/cmr_dbs_export_managed_instance.json";
+        static void Main(string[] args)
+        {
+            var json = File.OpenRead(jsonFile);
+            var document = JsonDocument.Parse(json);
+
+
+        }
+    }
+}
